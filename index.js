bf0ba8a22bdf4e1d2be06c2d8dda2848c496bfa8
Silvanos Eric <ericoderito@gmail.com>
2024-07-07 16:20:12 +0300

    feat: add utility functions for distance calculation and fare pricing
    - Implement `distanceFromHqInBlocks` and `distanceFromHqInFeet` to calculate distances from a fixed
      point.
    - Add `distanceTravelledInFeet` to compute the distance between two points in feet.
    - Create `calculatesFarePrice` function to calculate fare based on the distance travelled, with
      special pricing rules for different distance ranges.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const headquarters = 42;
+  return Math.abs(headquarters - someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+  const distanceInBlocks = distanceFromHqInBlocks(someValue);
+  return distanceInBlocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+  let distanceInFeet = distanceTravelledInFeet(start, destination); // ?
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    // First 400 feet are free; remove from distanceInFeet
+    distanceInFeet -= 400;
+
+    // Price in cents
+    const cents = distanceInFeet * 2;
+
+    // Price in dollars; 1 dollar = 1 cents
+    const dollars = cents / 100;
+    return dollars;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
